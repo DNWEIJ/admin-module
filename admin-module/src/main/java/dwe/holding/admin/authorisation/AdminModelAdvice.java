bbd92c5990f0cc72a91226daea49043dceb0637e
dwe <14996078+DNWEIJ@users.noreply.github.com>
2025-11-27 11:42:40 +0100

    initial version

--- admin-module/src/main/java/dwe/holding/admin/authorisation/AdminModelAdvice.java
+++ admin-module/src/main/java/dwe/holding/admin/authorisation/AdminModelAdvice.java
@@ -1,15 +1,18 @@
 package dwe.holding.admin.authorisation;
 
 import dwe.holding.admin.security.AutorisationUtils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice
+@Slf4j
 public class AdminModelAdvice {
     @ModelAttribute
     void modelAdvice(Model model) {
         try {
+            log.info("adminModelAdvice called");
             model.addAttribute("localMemberName", AutorisationUtils.getCurrentLocalMemberName());
             model.addAttribute("memberShortCode", AutorisationUtils.getCurrentMember().getShortCode());
             model.addAttribute("applicationView", AutorisationUtils.getCurrentMember().getApplicationView());
--- admin-module/src/main/java/dwe/holding/admin/authorisation/ControllerExceptionHandler.java
+++ admin-module/src/main/java/dwe/holding/admin/authorisation/ControllerExceptionHandler.java
@@ -2,16 +2,18 @@ package dwe.holding.admin.authorisation;
 
 import jakarta.servlet.http.HttpServletRequest;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice
+@Slf4j
 public class ControllerExceptionHandler {
-//    @ExceptionHandler(HsqlException.class)
-//    public String handleConstraintViolation(HsqlException ex, HttpServletRequest request, RedirectAttributes redirectAttributes) {
-//        redirectAttributes.addFlashAttribute("globalError", "Er is een fout opgetreden: deze gegevens bestaan al.");
-//        String referer = request.getHeader("Referer");
-//        return "redirect:" + referer;
-//    }
+    @ExceptionHandler(RuntimeException.class)
+    public String handleConstraintViolation(RuntimeException ex, RedirectAttributes redirectAttributes) {
+        log.error("Error occurred", ex.getCause());
+        redirectAttributes.addFlashAttribute("globalError", "Something went wrong!");
+        return "redirect:/admin/login";
+    }
 }
